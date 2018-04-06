558b086e588d437fb0b03a975cec91c8c87b7fa1
Gianluca Fioletti <gianluca.fioletti@smeup.com>
2018-04-06 16:39:19 +0200

    Add not found error handling

---
+++ src/main/java/com/smeup/tutorial/rest/NotFoundExceptionMapper.java
@@ -0,0 +1,20 @@
+package com.smeup.tutorial.rest;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+    @Override
+    public Response toResponse(final NotFoundException e) {
+
+        final Error error = new Error();
+        error.setError(e.getMessage());
+
+        return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON).entity(error).build();
+    }
+}
